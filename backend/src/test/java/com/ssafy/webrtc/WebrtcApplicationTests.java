3651012bb4f858369af64460abfde2f6dfd3c2a6
qospwmf <qospwmf@gmail.com>
2022-07-15 12:07:35 +0900

    🌵chore : init project

--- backend/src/test/java/com/ssafy/webrtc/WebrtcApplicationTests.java
+++ backend/src/test/java/com/ssafy/webrtc/WebrtcApplicationTests.java
@@ -2,12 +2,17 @@ package com.ssafy.webrtc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @SpringBootTest
+@Transactional
 class WebrtcApplicationTests {
+
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
