2a9181c3be9cae08c946bb3214b300ea36f43118
이상진 <berry2971@hanmail.net>
2022-07-21 11:20:18 +0900

    🥝Test : Add TURN Server test util
    - TURN Server의 정상 동작 여부를 확인할 수 있음

---
+++ backend-stun-turn/util/test-turn-server/test.js
@@ -0,0 +1,40 @@
+// https://stackoverflow.com/questions/28772212/stun-turn-server-connectivity-test
+
+const TURN_SERVER_URI = "turn:13.125.182.230:3478";
+
+function checkTURNServer(turnConfig, timeout) {
+  return new Promise(function (resolve, reject) {
+    let promiseResolved = false;
+
+    setTimeout(function () {
+      if (promiseResolved) return;
+      resolve(false);
+      promiseResolved = true;
+    }, timeout || 5000);
+
+    let myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection // compatibility for firefox and chrome
+    let pc = new myPeerConnection({ iceServers: [turnConfig] });
+    let noop = () => { };
+    pc.createDataChannel(""); // create a bogus data channel
+    pc.createOffer((sdp) => {
+      if (sdp.sdp.indexOf("typ relay") > -1) { // sometimes sdp contains the ice candidates
+        promiseResolved = true;
+        resolve(true);
+      }
+      pc.setLocalDescription(sdp, noop, noop);
+    }, noop); // create offer and set local description
+    pc.onicecandidate = (ice) => { // listen for candidate events
+      if (promiseResolved || !ice || !ice.candidate || !ice.candidate.candidate || !(ice.candidate.candidate.indexOf('typ relay') > -1)) return;
+      promiseResolved = true;
+      resolve(true);
+    };
+  });
+}
+
+checkTURNServer({
+  urls: TURN_SERVER_URI,
+  username: "berry",
+  credential: "straw"
+}).then(function (bool) {
+  console.log("Is TURN server active?", bool ? "YES" : "NO");
+});
