504485328f679bff7461e5322b996fd05a4c5a7b
berry2971 <berry2971@hanmail.net>
2022-07-23 00:24:01 +0900

    ⚡feat : notify users in room
    - 방 안에 있는 사람들에게 새로 접속한 회원 알려주기
    - 방 이름 설정 가능하게 하기

--- backend-room-server/public/js/client.js
+++ backend-room-server/public/js/client.js
@@ -1,14 +1,25 @@
 "use strict";
 
-const userName = (Math.random() + 1).toString(36).substring(7);
-const roomName = prompt("Type room name!");
-document.querySelector("#userName").innerHTML = userName;
-document.querySelector("#roomName").innerHTML = roomName;
-
 const socket = io.connect();
+
+const userName = (Math.random() + 1).toString(36).substring(7);
+document.querySelector("#userName").innerHTML = userName;
+
+const createRoomBtn = document.querySelector("#createRoomBtn");
+const joinRoomBtn = document.querySelector("#joinRoomBtn");
+
+createRoomBtn.addEventListener("click", (event) => {
+  const roomName = document.querySelector("#createRoomInput").value;
+  document.querySelector("#roomName").innerHTML = roomName;
+  socket.emit("create-room-req", { userName, roomName });
+});
 
 socket.emit("event", "data");
 
 socket.on("event", (data) => {
   // logic
 })
+
+socket.on("created-room-notify", (name) => {
+  console.log(`${name} has joined the room!!!`);
+});
--- backend-room-server/server.js
+++ backend-room-server/server.js
@@ -37,11 +37,18 @@ const io = new SocketServer(httpsServer);
 const rooms = {};
 
 io.sockets.on("connection", function(socket) {
-  socket.on("create-room-req", ({userName, roomName}) => {
+  socket.on("create-room-req", ({ userName, roomName }) => {
     const roomId = generateRandomUrlSafeString(20);
-    rooms[roomName] = {}
-    socket.join(roomName);
-    io.to(roomName).emit("", userName);
+    const users = new Set(); users.add(userName);
+    rooms[roomId] = {
+      roomName,
+      creator: userName,
+      created: Date.now(),
+      users
+    };
+    socket.join(roomId);
+    console.log(rooms);
+    io.to(roomId).emit("created-room-notify", userName);
     console.log("userName: ", userName, "roomName: ", roomName);
   });
 });
