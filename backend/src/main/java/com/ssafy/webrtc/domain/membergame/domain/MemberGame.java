de8f6341b4c995244a481924aeddaf22ea33f7c7
qospwmf <qospwmf@gmail.com>
2022-07-22 15:17:01 +0900

    ⚡feat: Create MemberGame Entity

---
+++ backend/src/main/java/com/ssafy/webrtc/domain/membergame/domain/MemberGame.java
@@ -0,0 +1,30 @@
+package com.ssafy.webrtc.domain.membergame.domain;
+
+import com.ssafy.webrtc.domain.game.domain.Game;
+import com.ssafy.webrtc.domain.member.domain.Member;
+
+import javax.persistence.*;
+
+@Entity
+public class MemberGame {
+
+    @Id @GeneratedValue
+    @Column(name = "member_game_id")
+    private Long id;
+
+    @Column(name = "win_cnt")
+    private Integer winCnt;
+
+    @Column(name = "lose_cnt")
+    private Integer loseCnt;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+
+}
