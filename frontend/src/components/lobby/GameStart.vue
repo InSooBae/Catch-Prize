5b4656341ece132618247a25b676a9b26c174daa
taehee <tevem1207@naver.com>
2022-07-22 18:07:13 +0900

    ⚡feat : Start modal 추가

---
+++ frontend/src/components/lobby/GameStart.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-form class="start-form" :model="gameinfo">
+    <el-form-item>
+      <el-input placeholder="방 제목을 입력하세요." v-model="gameinfo.roomname" />
+    </el-form-item>
+    <el-form-item>
+      <el-input placeholder="게임을 선택하세요." v-model="gameinfo.gametype" />
+    </el-form-item>
+    <el-form-item>
+      <el-input placeholder="참여인원을 선택하세요" v-model="gameinfo.gamesize" />
+    </el-form-item>
+    <el-button color="#00bd9d" type="info" class="start-button">GAME START!</el-button>
+  </el-form>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+const gameinfo = reactive({
+  roomname: '',
+  gametype: '',
+  gamesize: '',
+})
+</script>
--- frontend/src/components/lobby/SideBar.vue
+++ frontend/src/components/lobby/SideBar.vue
@@ -2,7 +2,7 @@
   <img src="@/assets/logo.svg" width="250" alt="logo" style="opacity: 0.9; margin: 0px 0px 0px 0px;">
   <el-tabs id="sidebar" tabPosition="bottom" stretch="true">
     <div class="d-flex sidebar-profile p-1 pt-2">
-      <img src="https://via.placeholder.com/50x50" alt="placeholder" style="margin-right: 10px;">
+      <img src="https://placekitten.com/50/50/" alt="placeholder" style="margin-right: 10px;">
       <div>
         <p>PlayerName</p>
         <p style="font-size: smaller; color: gray;">email@gmail.com</p>
@@ -33,18 +33,15 @@
       <SideBarSetProfile />
     </el-tab-pane>
   </el-tabs>
-  <el-button color="#00bd9d" size="large" type="info" class="start-button" @click="startVisible = true">GAME START!</el-button>
-  <el-dialog v-model="startVisible" :show-close="false">
-    <template #header="{ close, titleId, titleClass }">
+  <el-button color="#00bd9d" size="large" type="info" class="start-button" @click="startVisible = true">GAME START!
+  </el-button>
+  <el-dialog class="start-modal" v-model="startVisible" :show-close="false">
+    <template #header="{ titleId, titleClass }">
       <div class="my-header">
         <h4 :id="titleId" :class="titleClass">START YOUR GAME!</h4>
-        <el-button type="danger" @click="close">
-          <el-icon class="el-icon--left"><CircleCloseFilled /></el-icon>
-          Close
-        </el-button>
       </div>
     </template>
-    this is game info
+    <GameStart />
   </el-dialog>
 </template>
 
@@ -52,6 +49,7 @@
 import SideBarFriends from '@/components/lobby/SideBarFriends.vue'
 import SideBarMyRecord from '@/components/lobby/SideBarMyRecord.vue'
 import SideBarSetProfile from '@/components/lobby/SideBarSetProfile.vue'
+import GameStart from '@/components/lobby/GameStart.vue';
 
 import { ref } from 'vue'
 
@@ -59,122 +57,4 @@ const startVisible = ref(false)
 </script>
 
 <style>
-#sidebar {
-  background-color: #262C3A;
-  margin-bottom: 10px;
-  border-radius: 15px;
-}
-.sidebar-profile {
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  color: whitesmoke;
-  padding-bottom: 5px;
-  margin-bottom: 5px;
-  border-bottom: solid 1px;
-  border-color: #2c3e50;
-}
-.sidebar-element {
-  height: 50vh;
-}
-
-@media (max-height: 850px) {
-  .sidebar-element {
-    height: 46vh;
-  }
-}
-
-@media (max-height: 800px) {
-  .sidebar-element {
-    height: 44vh;
-  }
-}
-
-@media (max-height: 770px) {
-  .sidebar-element {
-    height: 43vh;
-  }
-}
-@media (max-height: 730px) {
-  .sidebar-element {
-    height: 42vh;
-  }
-}
-@media (max-height: 700px) {
-  .sidebar-element {
-    height: 37vh;
-  }
-}
-@media (max-height: 650px) {
-  .sidebar-element {
-    height: 31vh;
-  }
-}
-
-@media (max-height: 600px) {
-  .sidebar-element {
-    height: 26vh;
-  }
-}
-
-@media (max-height: 550px) {
-  .sidebar-element {
-    height: 17vh;
-  }
-}
-
-#sidebar>.el-tabs__content {
-  background-color: #262C3A;
-  padding: 15px;
-  border-radius: 15px;
-}
-
-.el-tabs__active-bar {
-  background-color: white;
-}
-
-.el-tabs__nav {
-  border-bottom: solid #0F1E33;
-  ;
-  padding: 0px 25px;
-}
-
-.el-tabs__item {
-  background-color: #262C3A;
-  padding: 0px 5px;
-  margin: 0px 5px;
-}
-
-.start-button {
-  border: none;
-  font-family: "PressStart2P";
-  font-size: medium;
-  width: 100%;
-  height: 60px;
-  border-radius: 10px;
-  color: white;
-}
-.start-button:hover {
-  color: gray;
-}
-.el-dialog {
-  border-radius: 10px;
-  border: solid #f2e53bCC;
-  background-color: #262C3A;
-}
-.el-dialog__title {
-  color: whitesmoke;
-}
-.el-dialog__body {
-  color: whitesmoke;
-}
-.el-form::-webkit-scrollbar {
-  width: 5px;
-}
-.el-form::-webkit-scrollbar-thumb {
-  background-color: #00bd9d;
-  border-radius: 5px;
-}
-.el-form::-webkit-scrollbar-track {
-  background-color: transparent;
-  border-radius: 5px;
-}
 </style>
