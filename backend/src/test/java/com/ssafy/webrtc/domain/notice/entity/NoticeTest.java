410db47b565625d6b0871127d6bc6f433bc54407
qospwmf <qospwmf@gmail.com>
2022-07-25 03:22:56 +0900

    🚗test : 생성 검증 테스트 코드 추가

--- backend/src/test/java/com/ssafy/webrtc/domain/notice/entity/NoticeTest.java
+++ backend/src/test/java/com/ssafy/webrtc/domain/notice/entity/NoticeTest.java
@@ -1,4 +1,23 @@
+package com.ssafy.webrtc.domain.notice.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class NoticeTest {
-  
+
+    @Test
+    @DisplayName("공지사항 생성 검증")
+    public void noticeCreateValid() throws Exception {
+        //given
+        Notice notice = Notice.of("test", "content");
+
+        assertAll(
+                () -> assertThat(notice.getTitle()).isEqualTo("test"),
+                () -> assertThat(notice.getContent()).isEqualTo("content")
+        );
+    }
+
 }
