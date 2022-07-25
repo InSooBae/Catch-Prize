c7e47beaa8216908faeb86d13b1998bf63199409
qospwmf <qospwmf@gmail.com>
2022-07-26 02:03:27 +0900

    ⚡feat : property-placeholder 적용

---
+++ backend/src/main/java/com/ssafy/webrtc/global/config/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.ssafy.webrtc.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 스프링 시큐리티 방지
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/token/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtExceptionFilter(),
+                        OAuth2LoginAuthenticationFilter.class)
+                .oauth2Login().loginPage("/token/expired")
+                .successHandler(successHandler)
+                .userInfoEndpoint().userService(oAuth2UserService);
+
+        http.addFilterBefore(new JwtAuthFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
