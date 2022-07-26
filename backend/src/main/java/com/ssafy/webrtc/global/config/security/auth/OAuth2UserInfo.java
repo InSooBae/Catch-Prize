6db1c9b9e081d1ab47a1947e8ff28ef86e4225c6
qospwmf <qospwmf@gmail.com>
2022-07-26 13:58:13 +0900

    ⚡feat : Naver, Google, Kakao OAuth 팩토리 패턴으로 및 인증 레파지토리 생성

---
+++ backend/src/main/java/com/ssafy/webrtc/global/config/security/auth/OAuth2UserInfo.java
@@ -0,0 +1,30 @@
+package com.ssafy.webrtc.global.config.security.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public abstract class OAuth2UserInfo {
+
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getName();
+
+    public abstract String getEmail();
+
+    public abstract String getImageUrl();
+
+}
