a0d7c402e813bfee866cb0f8f0a2a85c9b58c6e4
qospwmf <qospwmf@gmail.com>
2022-07-27 15:13:00 +0900

    ⚡feat : 예외 처리와 yml 변경

---
+++ backend/src/main/java/com/ssafy/webrtc/global/error/exception/OAuthProcessingException.java
@@ -0,0 +1,8 @@
+package com.ssafy.webrtc.global.error.exception;
+
+public class OAuthProcessingException extends RuntimeException {
+
+    public OAuthProcessingException(String message) {
+        super(message);
+    }
+}
