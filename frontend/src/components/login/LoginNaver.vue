604cc4ddce8cf27ce22c62c1072e1a35360eebe7
taehee <tevem1207@naver.com>
2022-07-28 14:05:20 +0900

    ⚡feat : naver oauth

---
+++ frontend/src/components/login/LoginNaver.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const naverLogin = new naver.LoginWithNaverId({
+    clientId: 'I4L7P1yrFjyL_fdoVw3i',
+    callbackUrl: `${window.location.origin}/callback/naver`,
+    isPopup: true,
+  })
+  naverLogin.init() 
+  naverLogin.reprompt()
+})
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,24 +5,55 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
     },
     {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('../views/SignupView.vue')
+      path: '/login',
+      name: 'login',
+      children: [
+        {
+          path: 'naver',
+          name: 'loginNaver',
+          component: () => import('../components/login/LoginNaver.vue'),
+        },
+      ],
+      component: () => import('../views/LoginView.vue')
     },
     {
       path: '/lobby',
       name: 'lobby',
-      component: () => import('../views/LobbyView.vue')
+      component: () => import('../views/LobbyView.vue'),
+      children: [
+        {
+          path: '',
+          name: 'main',
+          component: () => import('../components/lobby/RoomList.vue'),
+        },
+        {
+          path: ':gameid',
+          name: 'gameroom',
+          component: () => import('../components/lobby/WaitingRoom.vue'),
+        },
+      ],
     },
     {
       path: '/game',
       name: 'game',
       component: () => import('../views/GameView.vue')
     },
+    {
+      path: '/callback',
+      name: 'callback',
+      component: () => import('../views/CallbackView.vue'),
+      children: [
+        {
+          path: 'naver',
+          name: 'callbackNaver',
+          component: () => import('../components/callback/CallbackNaver.vue'),
+        },
+      ],
+    }
   ]
 })
 
