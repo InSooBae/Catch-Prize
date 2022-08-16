12e035839356f7a59a0a4ba2b7757b03149dd49f
soohong <5032303000@naver.com>
2022-08-17 01:52:31 +0900

    hobulho webcam

--- frontend/src/components/hobulho-game/HobulhoGame.vue
+++ frontend/src/components/hobulho-game/HobulhoGame.vue
@@ -11,15 +11,13 @@
 </template>
 
 <script setup>
-import { reactive, toRefs, inject } from "vue";
+import { reactive, inject, onBeforeUnmount, computed } from "vue";
 import { useRoute } from "vue-router";
 import HeaderHome from "./header/HeaderHome.vue";
 import LeftHome from "./main/LeftHome.vue";
 import RightHome from "./main/PlayersHome.vue";
-import { useStore } from "vuex";
-import { Data } from "phaser";
 
-// const store = useStore();
+
 const $clientstate = inject("$clientstate");
 const $hobulhoSocket = inject("$hobulhoSocket");
 const $dataBox = inject("$dataBox");
@@ -27,6 +25,10 @@ const $dataBox = inject("$dataBox");
 const route = useRoute();
 const myid = route.params.myid;
 const roomid = route.params.roomid;
+
+const roomId = sessionStorage.getItem('roomId')
+const token = { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
+
 $clientstate.myid = myid;
 $clientstate.roomid = roomid;
 
@@ -92,6 +94,112 @@ $hobulhoSocket.on("whose-judge", function () {
     $clientstate.gamestate = "judge-turn";
   }
 });
+
+
+const cam = reactive({
+	OV: undefined,
+	session: undefined,
+	mainStreamManager: undefined,
+	publisher: undefined,
+	subscribers: [],
+	mySessionId: computed(() => store.state.room.sessionId),
+	myUserName: computed(() => store.state.user.currentUser.username),
+})
+
+const joinSession = () => {
+  console.log(token)
+  console.log('ws connect')
+  
+	cam.OV = new OpenVidu();
+  // session을 사용할 수 있게함
+	cam.session = cam.OV.initSession();
+
+	cam.session.on('streamCreated', ({ stream }) => {
+		const subscriber = cam.session.subscribe(stream);
+		cam.subscribers.push(subscriber);
+	});
+	cam.session.on('streamDestroyed', ({ stream }) => {
+		const index = cam.subscribers.indexOf(stream.streamManager, 0);
+		if (index >= 0) {
+			cam.subscribers.splice(index, 1);
+		}
+	});
+	cam.session.on('exception', ({ exception }) => {
+		console.warn(exception);
+	});
+
+  // getToken에서 ovdata를 반환
+  // ovdata.token을 이용해서 session에 연결할 수 있음
+  getToken(roomId).then(ovdata => {
+
+  console.log(cam.myUserName)
+  cam.session.connect(ovdata.token)
+    .then(() => {
+      store.commit('SET_OV', ovdata)
+      sessionStorage.setItem('ovdata', JSON.stringify(ovdata))
+      // --- Get your own camera stream with the desired properties ---
+      let publisher = cam.OV.initPublisher(undefined, {
+        audioSource: undefined, // The source of audio. If undefined default microphone
+        videoSource: undefined, // The source of video. If undefined default webcam
+        publishAudio: true,  	// Whether you want to start publishing with your audio unmuted or not
+        publishVideo: true,  	// Whether you want to start publishing with your video enabled or not
+        resolution: '280x150',  // Cam Size
+        frameRate: 30,			// The frame rate of your video
+        insertMode: 'APPEND',	// How the video is inserted in the target element 'video-container'
+        mirror: false       	// Whether to mirror your local video or not
+      });
+      cam.mainStreamManager = publisher;
+      cam.publisher = publisher;
+      // --- Publish your stream ---
+      cam.session.publish(cam.publisher);
+    })
+    .catch(error => {
+      console.log('There was an error connecting to the session:', error.code, error.message);
+    });
+  })
+  // joinSession을 하면서 beforeunload 
+	window.addEventListener('beforeunload', leaveSession)
+}
+
+// 세션 퇴장
+const leaveSession = () => {
+  const roomId = sessionStorage.getItem('roomId')
+  const ovdata = JSON.parse(sessionStorage.getItem('ovdata'))
+  // 서버에 유저 삭제 요청
+  store.dispatch('removeUser', { roomId, ovdata})
+  .then(
+    ElMessage({
+      type: 'message',
+      message: '방에서 퇴장하셨습니다.'
+    })
+  )
+  // 초기화 및 이벤트 삭제
+  if (cam.session) cam.session.disconnect();
+    cam.session = undefined;
+    cam.mainStreamManager = undefined;
+    cam.publisher = undefined;
+    cam.subscribers = [];
+    cam.OV = undefined;
+    window.removeEventListener('beforeunload',leaveSession);
+}
+
+// roomId를 이용해서 ovtoken, userId 가져오는 요청 보내기
+const getToken = (roomId) => {
+  return new Promise((resolve, reject) => {
+    fetchRoomById({ Authorization: `Bearer ${sessionStorage.getItem('token')}` }, roomId)
+    .then(response => {
+      resolve(response.data)
+      })
+    .catch(error => reject(error.response))
+    })
+}    
+
+joinSession()
+
+// beforeunmount
+onBeforeUnmount(() => {
+  leaveSession()
+})
 </script>
 <style scoped>
 
