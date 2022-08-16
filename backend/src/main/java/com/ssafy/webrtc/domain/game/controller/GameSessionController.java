f01cc4777d47a1b5fc18ece33aa021e1d12fbab5
qospwmf <qospwmf@gmail.com>
2022-08-17 00:16:22 +0900

    ⚡feat : sse game start, game end

--- backend/src/main/java/com/ssafy/webrtc/domain/game/controller/GameSessionController.java
+++ backend/src/main/java/com/ssafy/webrtc/domain/game/controller/GameSessionController.java
@@ -5,6 +5,7 @@ import com.ssafy.webrtc.domain.game.dto.GameSessionLeaveRequestDto;
 import com.ssafy.webrtc.domain.game.dto.GameSessionRequestDto;
 import com.ssafy.webrtc.domain.game.dto.GameSessionResponseDto;
 import com.ssafy.webrtc.domain.game.entity.GameSession;
+import com.ssafy.webrtc.domain.game.enums.GameState;
 import com.ssafy.webrtc.domain.game.service.GameSessionService;
 import com.ssafy.webrtc.domain.member.MemberRepository;
 import com.ssafy.webrtc.global.security.auth.CustomUserDetails;
@@ -84,7 +85,7 @@ public class GameSessionController {
         return new ResponseEntity<GameSessionJoinResponseDto>(gameSessionJoinResponseDto, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "방 생성", notes = "<strong>RoomName, maxPlayer ...</strong>를 통해 방을 생성한다.")
+//    @ApiOperation(value = "방 생성", notes = "<strong>RoomName, maxPlayer ...</strong>를 통해 방을 생성한다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "401", description = "인증 실패"),
@@ -103,4 +104,24 @@ public class GameSessionController {
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
+    @GetMapping("/start/{roomId}")
+    public void gameStart(@PathVariable("roomId") String roomId, @ApiIgnore @AuthenticationPrincipal CustomUserDetails user) {
+        GameSession gameSession = gameSessionService.findById(roomId);
+        // 이미 시작했거나 방장이 아닌 다른 사람이 호출한거면
+        if (gameSession.getState() == GameState.STARTED || !user.getUsername().equals(gameSession.getHostName())) {
+            return;
+        }
+        gameSessionService.startSession(gameSession);
+    }
+
+    @GetMapping("/end/{roomId}")
+    public void gameEnd(@PathVariable("roomId") String roomId, @ApiIgnore @AuthenticationPrincipal CustomUserDetails user) {
+        GameSession gameSession = gameSessionService.findById(roomId);
+        // 이미 종료했거나 방장이 아닌 다른 사람이 호출한거면
+        if (gameSession.getState() == GameState.WAIT || !user.getUsername().equals(gameSession.getHostName())) {
+            return;
+        }
+
+        gameSessionService.endSession(gameSession);
+    }
 }
